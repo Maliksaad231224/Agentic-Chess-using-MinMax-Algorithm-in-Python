7d62d1812db7f49917a2faeb870ebc92bc012c1c
Maliksaad231224 <maliksaadahmed17@gmail.com>
2025-04-28 20:43:06 +0500

    made the board

---
+++ libraries.py
@@ -0,0 +1,120 @@
+
+import chess
+import pygame
+import random
+import math
+
+X = 800
+Y = 800
+screen = pygame.display.set_mode((X, Y))
+pygame.init()
+
+WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
+YELLOW = (204, 204, 0)
+BLUE = (50, 255, 255)
+BLACK = (0, 0, 0)
+
+
+b = chess.Board()
+PIECE_SIZE = 100
+
+def load_and_scale_piece(path):
+    try:
+        piece = pygame.image.load(path).convert_alpha()
+        return pygame.transform.scale(piece, (PIECE_SIZE, PIECE_SIZE))
+    except:
+        print(f"Error loading image: {path}")
+       
+        surface = pygame.Surface((PIECE_SIZE, PIECE_SIZE))
+        surface.fill((255, 0, 0))  
+        return surface
+
+pieces = {
+    'p': load_and_scale_piece('images/black_pawn.png'),
+    'n': load_and_scale_piece('images/black_knight.png'),
+    'b': load_and_scale_piece('images/black_bishop.png'),
+    'r': load_and_scale_piece('images/black_rook.png'),
+    'q': load_and_scale_piece('images/black_queen.png'),
+    'k': load_and_scale_piece('images/black_king.png'),
+    'P': load_and_scale_piece('images/white_pawn.png'),
+    'N': load_and_scale_piece('images/white_knight.png'),
+    'B': load_and_scale_piece('images/white_bishop.png'),
+    'R': load_and_scale_piece('images/white_rook.png'),
+    'Q': load_and_scale_piece('images/white_queen.png'),
+    'K': load_and_scale_piece('images/white_king.png'),
+}
+
+def update(screen, board):
+    for i in range(64):
+        piece = board.piece_at(i)
+        if piece ==None:
+            pass
+        else:
+            screen.blit(pieces[str(piece)], ((i%8)*100, 700-(i//8)*100))
+
+    for i in range(7):
+        i = i+1
+        pygame.draw.line(screen, WHITE, (0, i*100), (800, i*100))
+        pygame.draw.line(screen, WHITE, (i*100,0), (i*100,800))
+    
+    pygame.display.flip()
+
+def main(BOARD):
+    screen.fill(BLACK)
+    pygame.display.set_caption('AI Powered Chess')
+
+    index_moves = []
+    status = True
+    while(status):
+        update(screen, BOARD)
+
+        for event in pygame.event.get():
+            if event.type ==pygame.QUIT:
+                status = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                screen.fill(BLACK)
+                #positio n of mouse
+                pos = pygame.mouse.get_pos()
+                #calculating clicked square's index
+                 #divide x axis by 100 to get column number
+                #divide y axis by 100 to get row number
+                # rounds down to the nearest integer 
+                square = (math.floor(pos[0]/100), math.floor(pos[1]/100))
+                #calculate the row number from top. since origin is top-left, we need to invert the y- coordinates. by subtracting square[1] from 7 we get row number from top
+                #*8 this multiplied row number by 8, calculating the starting index of row
+                # + square[0] adds the column number to row starting index giving us index of square
+                index = (7-square[1]*8+(square[0]))
+
+                if index in index_moves:
+                    move = moves[index_moves.index(index)]
+                    BOARD.push(move)
+                    index=None
+                    index_moves = []
+
+                else:
+                    piece = BOARD.piece_at(index)  # checks if there is a piece on clicked square
+
+                    if piece == None:
+                        pass # on empty
+                    else:
+                        all_moves = list(BOARD.legal_moves)
+                        moves = []
+                        for m in all_moves:
+                            if m.from_square == index:
+                                moves.append(m)
+                                #highlight legal squares
+                                t = m.to_square
+                                TX1 = 100*(t%8)
+                                TY1 = 100*(7-t//8)
+
+                                pygame.draw.rect(screen, BLUE, pygame.Rect(TX1, TY1, 100, 100),5)
+
+                        index_moves = [a.to_square for a in moves]
+
+        if BOARD.outcome() !=None:
+            print(BOARD.outcome())
+            status=False
+            print(BOARD)
+    pygame.quit()
